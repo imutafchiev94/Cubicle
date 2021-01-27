3070ee4693bd58a1c913a5cf53cd807ee27cca7f
Ivaylo Mutafchiev <stu1901322006@uni-plovdiv.bg>
2021-01-27 14:22:24 +0200

    Extract Express Config

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
-const config = require('./config/config')
 
+const config = require('./config/config')
+const expressConfig = require('./config/express');
 const app = express();
 
-console.log(process.env.NODE_ENV);
+expressConfig(app);
+
+app.get('/', (req, res) =>{
+    res.render('home', {layout: false});
+})
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
